23823efd1d50273eab8af6486fa34726a5bf04ae
Srikanth R Tiyyaguru <srikanth@srtweb.onmicrosoft.com>
2019-04-19 21:02:13 -0400

    Initial Commit

---
+++ src/webparts/eRfaComp/components/IERfaCompProps.ts
@@ -0,0 +1,3 @@
+export interface IERfaCompProps {
+  description: string;
+}
---
+++ src/webparts/eRfaComp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IERfaCompWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ERfaCompWebPartStrings' {
+  const strings: IERfaCompWebPartStrings;
+  export = strings;
+}
